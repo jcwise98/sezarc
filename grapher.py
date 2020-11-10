1be25115bf2e46b9f35092085238951c057d88a5
Derek-Baum <dgbaum@udel.edu>
2020-11-09 23:34:07 -0500

    Added heat_map function TODO: Generalize

--- grapher.py
+++ grapher.py
@@ -2,7 +2,6 @@ import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 # Filters a pandas 'data_frame' for values with the date between 'start' and 'end'
 # date_column is the name of the column storing the date (default 'Date')
 # Returns the filtered data frame between the 2 given dates
@@ -21,6 +20,21 @@ def create_frequency_data(data_frame, column):
     y = data_frame[column].value_counts()
     return x, y
 
+def heat_map_plot(self, file):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    data = pd.read_excel(file)
+
+    data = data.loc[(data['Channel Type'] == 'Interval') &
+                    (pd.notnull(data['Space Use Coordinate X'])) &
+                    (pd.notnull(data['Space Use Coordinate Y'])) &
+                    (pd.notnull(data['Depth in Meters']))]
+
+    x = data['Space Use Coordinate X'].values.flatten()
+    y = data['Space Use Coordinate Y'].values.flatten()
+    z = data['Depth in Meters'].values.flatten()
+    ax.scatter(x, y, z)
+    fig.show()
 
 # Filters the given data frame with entries only between start and end
 # Creates a frequency bar plot and saves it with the given 'output' name
@@ -36,13 +50,15 @@ def create_frequency_figure(data_frame, column, start, end, output):
     plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
     for bar in bars:
         height = bar.get_height()
-        ax.text(bar.get_x() + bar.get_width()/2, height, '%d' % int(height), ha='center', va='bottom')
+        ax.text(bar.get_x() + bar.get_width() / 2, height, '%d' % int(height), ha='center', va='bottom')
 
     fig.savefig(output, bbox_inches='tight')
     plt.show()
 
 
 if __name__ == "__main__":
+    # heat_map_plot("MarinelandThru30June2020_FormattedWithDepth.xlsx")
+    #
     parser = argparse.ArgumentParser(description='Create a graph of data frequencies, filterable by date')
     parser.add_argument('file', type=str, help='the path to the excel file')
     parser.add_argument('column', type=str, help='the column name to collect data from in the excel file')
